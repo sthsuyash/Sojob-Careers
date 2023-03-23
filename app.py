6094ec03e837af1e6454c65217288f190cbcb32b
Suyash Shrestha <sthasuyash11@gmail.com>
2023-03-23 13:47:58 +0545

    initital commit

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
+# database variables
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Data Analyst',
+        'location': 'Kathmandu, Nepal',
+        'salary': 'Rs. 1,00,000'
+    },
+    {
+        'id': 2,
+        'title': 'Data Scientist',
+        'location': 'Pokhara, Nepal',
+        'salary': 'Rs. 2,00,000'
+    },
+    {
+        'id': 3,
+        'title': 'Frontend Developer',
+        'location': 'Remote',
+    },
+    {
+        'id': 4,
+        'title': 'Backend Developer',
+        'location': 'LA, USA',
+        'salary': '$1,20,000'
+    },
+
+]
+
+
+@app.route('/')
+def index():
+    # return '<h1>Hello World!</h1>'
+    return render_template('index.html', jobs=JOBS, company_name="Sojob")
+
+@app.route('/jobs')
+def list_jobs():
+    return jsonify(JOBS)
+
+
+if __name__ == '__main__':
+    app.run(port=8000, debug=True)
