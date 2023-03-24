c24ad20c6f0c3598ef0ab906bb23e9447950660f
Suyash Shrestha <sthasuyash11@gmail.com>
2023-03-24 13:43:55 +0545

    add database logic file

---
+++ database.py
@@ -0,0 +1,29 @@
+from sqlalchemy import create_engine, text
+import config
+
+db_connection_string = config.DATABASE_CONNECTION
+engine = create_engine(
+    db_connection_string,
+    connect_args={
+        "ssl": {
+        "ssl_ca": "/etc/ssl/cert.pem"
+        }
+    })
+
+def load_jobs_from_db():
+    with engine.connect() as con:
+        result = con.execute(text("SELECT * FROM jobs"))
+        # for each_job in result:
+        #     print(f"\n\nJob id: {each_job[0]}")
+        #     print(f"Name: {each_job[1]}\nLocation: {each_job[2]}\nSalary: {each_job[4]}.{each_job[3]}\nDescription: {each_job[5]}\nRequirements: {each_job[6]}")
+
+        jobs=[]
+        for row in result.all():
+            jobs.append(dict(zip(result.keys(), row)))
+        return jobs
+
+        # first_result = result.fetchone()
+        # if first_result:
+        #     first_result_dict = dict(zip(result.keys(), first_result))
+        
+        # print(first_result_dict['title'])
