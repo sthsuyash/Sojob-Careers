9e224a093b3d80044041ce932f85dcafa854ca0c
Suyash Shrestha <sthasuyash11@gmail.com>
2023-03-24 14:22:26 +0545

    update database

--- app.py
+++ app.py
@@ -1,48 +1,52 @@
 from flask import Flask, render_template, jsonify
+from database import load_jobs_from_db
 
 app = Flask(__name__)
 
 # database variables
-JOBS = [
-    {
-        'id': 1,
-        'title': 'Data Analyst',
-        'location': 'Kathmandu, Nepal',
-        'salary': 'Rs. 1,00,000'
-    },
-    {
-        'id': 2,
-        'title': 'Data Scientist',
-        'location': 'Pokhara, Nepal',
-        'salary': 'Rs. 2,00,000'
-    },
-    {
-        'id': 3,
-        'title': 'Frontend Developer',
-        'location': 'Remote',
-    },
-    {
-        'id': 4,
-        'title': 'Backend Developer',
-        'location': 'LA, USA',
-        'salary': '$1,20,000'
-    },
+# JOBS = [
+#     {
+#         'id': 1,
+#         'title': 'Data Analyst',
+#         'location': 'Kathmandu, Nepal',
+#         'salary': 'Rs. 1,00,000'
+#     },
+#     {
+#         'id': 2,
+#         'title': 'Data Scientist',
+#         'location': 'Pokhara, Nepal',
+#         'salary': 'Rs. 2,00,000'
+#     },
+#     {
+#         'id': 3,
+#         'title': 'Frontend Developer',
+#         'location': 'Remote',
+#     },
+#     {
+#         'id': 4,
+#         'title': 'Backend Developer',
+#         'location': 'LA, USA',
+#         'salary': '$1,20,000'
+#     },
 
-]
+# ]
 
 
 @app.route('/')
 def index():
     # return '<h1>Hello World!</h1>'
-    return render_template('index.html', jobs=JOBS, company_name="Sojob")
+    jobs = load_jobs_from_db()
+    return render_template('index.html', jobs=jobs, company_name="Sojob")
 
-@app.route('/jobs')
+@app.route('/api/jobs')
 def list_jobs():
-    return jsonify(JOBS)
+    jobs = load_jobs_from_db()
+    return jsonify(jobs)
 
-@app.route('/jobs/<int:id>')
+@app.route('/api/jobs/<int:id>')
 def get_job(id):
-    job = [job for job in JOBS if job['id'] == id]
+    jobs = load_jobs_from_db()
+    job = [job for job in jobs if job['id'] == id]
     if len(job) == 0:
         return jsonify({'message': 'Job not found!'})
     return jsonify(job[0])
