7002fb34d181e1a5cb3d988195d421385195c98f
Suyash Shrestha <sthasuyash11@gmail.com>
2023-03-24 23:57:09 +0545

    fixed connection

--- database.py
+++ database.py
@@ -1,15 +1,18 @@
 from sqlalchemy import create_engine, text
+from dotenv import load_dotenv
+load_dotenv()
 import os
 
-db_connection_string = os.getenv['DATABASE_CONNECTION']
+db_connection_string = f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}?charset=utf8mb4"
 
 engine = create_engine(
     db_connection_string,
     connect_args={
         "ssl": {
-        "ssl_ca": "/etc/ssl/cert.pem"
+            "ssl_ca": "/etc/ssl/cert.pem"
         }
     })
+
 
 def load_jobs_from_db():
     with engine.connect() as con:
@@ -18,7 +21,7 @@ def load_jobs_from_db():
         #     print(f"\n\nJob id: {each_job[0]}")
         #     print(f"Name: {each_job[1]}\nLocation: {each_job[2]}\nSalary: {each_job[4]}.{each_job[3]}\nDescription: {each_job[5]}\nRequirements: {each_job[6]}")
 
-        jobs=[]
+        jobs = []
         for row in result.all():
             jobs.append(dict(zip(result.keys(), row)))
         return jobs
@@ -26,5 +29,5 @@ def load_jobs_from_db():
         # first_result = result.fetchone()
         # if first_result:
         #     first_result_dict = dict(zip(result.keys(), first_result))
-        
+
         # print(first_result_dict['title'])
